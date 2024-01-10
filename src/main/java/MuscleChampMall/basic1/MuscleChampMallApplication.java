25fa653e8c5288f682b8a4feb1fe32dc52ba0a65
tjqls <tjqls2013@gmail.com>
2024-01-10 19:34:55 +0900

    build

---
+++ src/main/java/MuscleChampMall/basic1/MuscleChampMallApplication.java
@@ -0,0 +1,13 @@
+package MuscleChampMall.basic1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuscleChampMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuscleChampMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MuscleChampMall/basic1/MuscleChampMallApplicationTests.java
@@ -0,0 +1,13 @@
+package MuscleChampMall.basic1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuscleChampMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
