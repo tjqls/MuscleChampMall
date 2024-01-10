6922098d9cb8513ea2ffad892f4b89e3e445c04c
kael0595 <changgyu549@gmail.com>
2024-01-10 21:11:18 +0900

    detail 작성

--- src/main/java/MuscleChampMall/Review/Controller/ReviewController.java
+++ src/main/java/MuscleChampMall/Review/Controller/ReviewController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -25,5 +26,16 @@ public class ReviewController {
     model.addAttribute("reviewList", reviewList);
 
     return "review/list";
+  }
+
+  @GetMapping(value = "/detail/{id}")
+  public String detail(Model model,
+                       @PathVariable("id") Long id){
+
+    Review review = this.reviewService.findById(id);
+
+    model.addAttribute("review", review);
+
+    return "review/detail";
   }
 }
--- src/main/java/MuscleChampMall/Review/Service/ReviewService.java
+++ src/main/java/MuscleChampMall/Review/Service/ReviewService.java
@@ -1,11 +1,13 @@
 package MuscleChampMall.Review.Service;
 
+import MuscleChampMall.Base.Exception.DataNotFoundException;
 import MuscleChampMall.Review.Entity.Review;
 import MuscleChampMall.Review.Repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,4 +19,10 @@ public class ReviewService {
    return this.reviewRepository.findAll();
   }
 
+  public Review findById(Long id) {
+    Optional<Review> review = this.reviewRepository.findById(id);
+    if (review.isPresent()){
+      return review.get();
+    } throw new DataNotFoundException("review not found");
+  }
 }
